cc6f4ab7ad9d1bd63c51dabb620693de169ba97e
GaripovRinat <rin.garipoff995@outlook.com>
2025-01-15 23:28:03 +0400

    Add files via upload
    Этап 1 Задание №1 "Переменные"

---
+++ greetings.py
@@ -0,0 +1,10 @@
+username = "Plan orders"
+title = "Application execution plan"
+content = "Sales of 5,200 rubles"
+status1 = "Completed"
+status2 = "Not comleted"
+created_date = "15-01-2025"
+issue_date = "18-01-2025"
+print(username, title, sep=',')
+print(created_date, issue_date)
+print(content, status1, sep=',')
